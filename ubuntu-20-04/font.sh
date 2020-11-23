7334fc70127748e847a1da8e015312394a09fc22
Juarez <jz@brickabode.com>
2020-11-23 12:22:02 -0300

    Add script to download fura font

---
+++ ubuntu-20-04/font.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+
+mkdir -p .local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraMono/Regular/complete/Fura%20Mono%20Regular%20Nerd%20Font%20Complete.otf
